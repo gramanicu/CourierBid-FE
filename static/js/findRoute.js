db6e8909ee18820c0d440433e140064a135d01bf
Grama Nicolae <gramanicu@gmail.com>
2022-01-07 15:05:58 +0200

    Small optimization

--- static/js/findRoute.js
+++ static/js/findRoute.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', () => {
-    console.log('ASDKJHAS');
+    if (document.querySelectorAll('#arcgis-map').length > 0) {
+        require(['esri/config', 'esri/Map', 'esri/views/MapView'], function (esriConfig, Map, MapView) {
+            esriConfig.apiKey = sessionStorage.getItem('esriApiKey');
 
-    require(['esri/config', 'esri/Map', 'esri/views/MapView'], function (esriConfig, Map, MapView) {
-        esriConfig.apiKey = sessionStorage.getItem('esriApiKey');
+            const map = new Map({
+                basemap: 'arcgis-topographic', // Basemap layer service
+            });
 
-        const map = new Map({
-            basemap: 'arcgis-topographic', // Basemap layer service
+            const view = new MapView({
+                map: map,
+                center: [-118.805, 34.027], // Longitude, latitude
+                zoom: 13, // Zoom level
+                container: 'arcgis-map', // Div element
+            });
         });
-
-        const view = new MapView({
-            map: map,
-            center: [-118.805, 34.027], // Longitude, latitude
-            zoom: 13, // Zoom level
-            container: 'arcgis-map', // Div element
-        });
-    });
+    }
 });
