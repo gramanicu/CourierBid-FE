d0913d5061618daffdd797b1a8a4914da6ce17a1
Grama Nicolae <gramanicu@gmail.com>
2022-01-07 15:17:35 +0200

    Trying more fixes

--- static/js/findRoute.js
+++ static/js/findRoute.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
 });
 
 function loadMap() {
-    if (sessionStorage.getItem('esriApiKey')) {
+    if (sessionStorage.getItem('esriApiKey') != '') {
         require(['esri/config', 'esri/Map', 'esri/views/MapView'], function (esriConfig, Map, MapView) {
             esriConfig.apiKey = sessionStorage.getItem('esriApiKey');
 
